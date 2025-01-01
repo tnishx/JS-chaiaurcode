e64da383c628f1de5e66fb11c78ac07701f1e8a1
tnishx <tanishsingh797@gmail.com>
2025-01-01 13:46:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+ 
+// alert( 3 + 3); // alert doesn't work in node only browser
+
+console.log(3 + 3)
+
+console.log("darth")
+
+let name = "darth";
+let age = 7;
+let isLogged = false ;
+
+// primitive data types
+
+// number    => 2^53 (only upto this no.)
+// bigint
+// string    => ""
+// boolean   => true/false
+// null      => standalone value(empty value) *special(it's also object)
+// undefined => no value assigned 
+// symbol    => unique
+
+
+// object
+
+console.log(typeof "darth"); //value
+console.log(typeof age);     //variable
+console.log(typeof null);    //datatype
