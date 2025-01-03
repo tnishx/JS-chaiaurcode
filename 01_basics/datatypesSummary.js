0a492aa1a51e2ab77a49401f3b59c05652278279
tnishx <tanishsingh797@gmail.com>
2025-01-03 19:29:17 +0000

    memory done

--- 01_basics/datatypesSummary.js
+++ 01_basics/datatypesSummary.js
@@ -31,3 +31,32 @@ const myFunction = function(){
 console.table([typeof heros, typeof myObj, typeof myFunction]);
 
 // type of (array and object => object) (function => object function)
+
+
+//***********************************************************memory***************************************************************
+
+// stack => (primitive), Heap => (refrence/non-primitive)
+// in stack we get duplicate value so change is applied to only the variable which we're changing
+// in heap we get refrence of the original value so the change made is apllied to both varibale
+
+/*Stack*/
+let myFootballName = "CAFU"
+let myAnotherName = myFootballName
+myAnotherName = "MALDINI"
+
+console.log(myFootballName)
+console.log(myAnotherName)
+
+/*Heap*/
+let userOne = {
+    name: "ramos",
+    email: "ramos@brixit",
+}
+
+let userTwo = userOne
+
+userTwo.email = "walker@fragile"
+
+console.log(userOne)
+console.log(userTwo)
+
